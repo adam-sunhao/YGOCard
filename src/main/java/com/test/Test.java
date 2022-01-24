9c0d8fe14a7df0de026b83004f4905796f7e2a98
sunhao <adam_sunhao@163.com>
2022-01-24 16:48:52 +0800

    优化数据库操作

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -2,20 +2,18 @@ package com.test;
 
 
 import com.test.entity.YGOCard;
+import com.test.util.YGOUtil;
 
 import java.util.List;
 
 public class Test {
-    public static void main(String[] args) throws ClassNotFoundException {
-        //List<YGOCard> cardByName = YGOUtil.getCardByName("eye",YGOUtil.enDBType);
-        List<YGOCard> cardByName = YGOUtil.getCardByName("究極亜竜",YGOUtil.jaDBType);
-        for (YGOCard ygoCard:cardByName){
-           // System.out.println(ygoCard);
-            YGOCard ygoCardCH = YGOUtil.getCardById(ygoCard.getId(),YGOUtil.zhDBType);
-            System.out.println(ygoCardCH.getName());
-            System.out.println(ygoCardCH.getDesc());
-            System.out.println(ygoCardCH);
-        }
+    public static void main(String[] args) {
+        YGOCard cardInfo = YGOUtil.getCardById(10000);
+        System.out.println(cardInfo);
 
+        List<YGOCard> cards = YGOUtil.getCardsByName("龙");
+        for (YGOCard card : cards) {
+            //System.out.println(card);
+        }
     }
 }
